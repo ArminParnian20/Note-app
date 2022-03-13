a789afaa89607b86bd18ab6926b671221ccf185f
Armin <arminparnian182@gmail.com>
2022-03-13 17:13:59 +0330

    23

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import NoteList from './Components/NoteList';
 import simpleContext from './context';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import Header from './Components/Header';
+import { BarLoader, BeatLoader, BounceLoader } from 'react-spinners';
+
 const App = () => {
   const [noteText,setNoteText]=useState('');
   const [searchText,setSearchText]=useState('');
   const [darkMode,setDarkMode]=useState(false);
-  const [notes,setNotes]=useState([])
-  
+  const [notes,setNotes]=useState([]);
+  const [loadig,setloading]=useState(false);
+  window.addEventListener('load',
+  ()=>{setTimeout(()=>setloading(true),1000)}
+  )
   useEffect(()=>{
     const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
     if(savedNotes){
@@ -28,8 +33,13 @@ const App = () => {
   noteText:noteText,searchText:searchText,
   setDarkMode:setDarkMode,setSearchText:setSearchText}}>
   <div className='cont' style={{'--i':`${darkMode ? '#333':"#ddd"}`}}>
-   <Header/>
-  <NoteList/>
+{loadig ? <div>   <Header/>
+  <NoteList/></div>:   <BounceLoader  loading color='red'/>}
+
+   
+
+
+  
   </div>
 
   </simpleContext.Provider>
